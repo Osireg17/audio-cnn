340e153f956729abdbdae6fa69014177286c0950
Osi Obomighie <93836683+Osireg17@users.noreply.github.com>
2025-08-28 11:57:59 +0100

    Add audio CNN model implementation and training script
    - Implement ResidualBlock and AudioCNN classes in model.py
    - Create a training script in train.py with Modal integration
    - Update requirements.txt for necessary dependencies

---
+++ train.py
@@ -0,0 +1,38 @@
+import sys
+
+import modal
+
+app = modal.App("audio-cnn")
+
+image = (modal.Image.debian_slim()
+         .pip_install_from_requirements("requirements.txt")
+         .apt_install(["wget", "unzip", "ffmpeg", "libsndfile1"])
+         .run_commands([
+             "cd /tmp && wget https://github.com/karolpiczak/ESC-50/archive/master.zip -O esc50.zip",
+             "cd /tmp && unzip esc50.zip",
+             "mkdir -p /opt/esc50-data",
+             "cp -r /tmp/ESC-50-master/* /opt/esc50-data/",
+             "rm -rf /tmp/esc50.zip /tmp/ESC-50-master"
+         ])
+         .add_local_python_source("model"))
+
+volume = modal.Volume.from_name("esc50-data", create_if_missing=True)
+modal_volume = modal.Volume.from_name("esc-model", create_if_missing=True)
+
+@app.function()
+def f(i):
+    if i % 2 == 0:
+        print("hello", i)
+    else:
+        print("world", i, file=sys.stderr)
+
+    return i * i
+
+@app.local_entrypoint()
+def main():
+    # run the function locally
+    print(f.local(10))
+
+    # run the function remotely on Modal
+    print(f.remote(10))
+
